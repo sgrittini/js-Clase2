a0f95d24c8720098a56af6804d2ada5d04a4c8cc
Sergio Grittini <106285902+sgrittini@users.noreply.github.com>
2022-07-14 15:29:13 -0300

    first commit

--- app.js
+++ app.js
@@ -1,67 +1,83 @@
 
-function saludar()
-{   
+function saludar() {
 
     mesage("hola");
 }
 
-function saludarSergio()
-{
-     mesage("hola Sergio");
+function saludarSergio() {
+    mesage("hola Sergio");
 }
 
 function saludarA(nombre) {
-   
-   // mesage("hola "+nombre);
+
+    // mesage("hola "+nombre);
     mesage(`hola ${nombre}`);
-  
+
 }
 
 function saludarANN() {
     let nombre = prompt("Como te llamas?");
-    mesage("hola "+nombre);
-    
+    mesage("hola " + nombre);
+
 }
 
-function sumar(){
+function sumar() {
     let entrada1 = prompt("ingresa un nro");
     let entrada2 = prompt("ingresa otro nro");
-    
-    if(isNaN(entrada1)||isNaN(entrada2))
-    {
+
+    if (isNaN(entrada1) || isNaN(entrada2)) {
         mesage("no se puede sumar, uno de los dos valores no es un nro");
     }
-    else{
-        mesage("El resultado es: "+(parseInt(entrada1)+parseInt(entrada2)));
+    else {
+        mesage("El resultado es: " + (parseInt(entrada1) + parseInt(entrada2)));
     }
+
+}
+
+function concatenar() {
+    let entrada1 = prompt("ingresa un valor");
+    let entrada2 = prompt("ingresa otro valor");
+    mesage("El valor concatenado es: " + entrada1 + entrada2);
+
 
 }
 
 function mesage(mensaje) {
-    
-    if(document.getElementById('Alert').checked)
-    {
+    /*
+    Evalua que check esta checkeado
+    los mensaje se muestran por cualquiera de estas combinaciones
+    * Alert
+    * Console
+    * en la Pagina
+    */
+
+    if (document.getElementById('Alert').checked) {
         alert(mensaje);
     }
-
-    if(document.getElementById('Console').checked)
-    {
+    
+    if (document.getElementById('Console').checked) {
         console.log(mensaje);
     }
-    
-    
-    
-    let p = document.getElementById("pResultado");
-    if (p!=null)
-     {
-         let padre = p.parentNode;
-         padre.removeChild(p);
-     }
 
-    let parent = document.getElementById("resultado");
-    let elemento = document.createElement('p');
-    elemento.setAttribute("id","pResultado");
-    elemento.innerText=mensaje;
-    
-    parent.appendChild(elemento);
+    //Siempre borra de la pagina por si hay algun mensaje
+    //Si existe un pResultado el mismo es cargado por el JS
+    //de existir recuper el padre y borra todos los hijos.
+    let p = document.getElementById("pResultado");
+    if (p != null) {
+        let padre = p.parentNode;
+        padre.removeChild(p);
+    }
+
+    /*si el check page esta chequeado, busca el id contenedor de resultado
+    crea un p y añade el mensaje
+    */
+    if (document.getElementById('Page').checked) {
+        let parent = document.getElementById("resultado");
+        let elemento = document.createElement('p');
+        elemento.setAttribute("id", "pResultado");
+        elemento.innerText = mensaje;
+
+        parent.appendChild(elemento);
+    }
+
 }
